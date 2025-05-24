4f698e6b7444b8b9d6da02b38d0bf8c65a5daefb
JuniorAlba <juninhoalba@gmail.com>
2025-05-24 17:09:58 -0300

    Ejercicio 6 mod

--- TP5/eje6.m
+++ TP5/eje6.m
@@ -4,9 +4,8 @@ y = [0 4 22/3];
 [a,b,c,d] = cubic_spline_clamped(x,y,3,3);
 p = x;
 S = @(x) a(1)*(x==p(1));
-for i=2:length(p)
-    S = @(x) S(x) + ...
-    polyval([d(i-1) c(i-1) b(i-1) a(i-1)], (x - p(i-1))).*(x>p(i-1)).*(x<=p(i));
+for i=1:length(p)-1
+    S = @(x) S(x) + polyval([d(i) c(i) b(i) a(i)], (x - p(i))).*(x>p(i)).*(x<=p(i+1));
 endfor
 
 
@@ -17,6 +16,11 @@ hold on;
 plot(xx,S(xx),'k-','linewidth',3)
 grid on
 grid minor
+
+a1=a
+b1=b
+c1=c
+d1=d
 
 %recordar que spline_clamped(x,y,0,0) != spline_natural(x,y)
 % ya que el spline clamped impone a las primeras derivadas de los extremos que sean 0
