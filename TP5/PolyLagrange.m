7737a2a860ca520de2efc5e153301609ed7ded25
JuniorAlba <juninhoalba@gmail.com>
2025-05-24 13:47:44 -0300

    Ejercicio5 mod

--- TP5/PolyLagrange.m
+++ TP5/PolyLagrange.m
@@ -1,17 +1,24 @@
 %P: vector de coeficientes del polinomio de Lagrange que interpola los puntos (x,y)
 %x: valores que toman los puntos interpolantes
 %y: valores de la funcion interpolada en los puntos x[i]
-function [P] = PolyLagrange(x,y)
+function [P] = PolyLagrange(x, y)
   n = length(x);
-  P = zeros(1,1);
-  for i=1:n
-    for j=1:n
-      num = 1;
-      denom = 1;
-      if i!=j
-        num = conv(num, [1 (-1)*x(j)]);
-        denom = denom * (x(i)-x(j));
+  P = zeros(1, n);
+
+  for i = 1:n
+    num = [1];
+    denom = 1;
+    for j = 1:n
+      if j != i
+        num = conv(num, [1 -x(j)]);  % Multiplicar polinomios: L *= (x - x_j)
+        denom = denom * (x(i) - x(j));
       endif
     endfor
-    P = P + y(i)*(num./denom);
+
+    % Asegurar que L tenga longitud n (rellenar con ceros a la derecha)
+    num = [num, zeros(1, n - length(num))];
+
+    % Sumar el término y_i * L_i(x) / denom al polinomio final
+    P = P + (y(i) / denom) * num;
   endfor
+end
---
+++ TP5/eje5_datosperturbados.m
@@ -0,0 +1,50 @@
+
+
+display("Ahora con datos perturbados")
+f = @(x) sin(2*pi.*x)+(-1).^[1:21]*1e-04;
+x = linspace(-1,1,21);
+xx = linspace(-1,1,201);
+y = f(x(1:21));
+%tambien es posible encontrar el poly de lagrange utilizando polyfit con grad=20
+%Es posible gracias al teorema de existencia y unicidad
+[P] = PolyLagrange(x,y);
+[a,b,c,d] = cubic_spline_natural(x,y);
+
+y_LAG = polyval(P,xx);
+
+figure(1);
+hold on;
+plot(x,f(x),'r-','linewidth',6);
+plot(xx,y_LAG,'g-','linewidth',5)
+
+
+n = length(a);
+xd = x;
+M = [d c b a];
+S = @(x) a(1)*(x == xd (1));
+for i=1:n
+ S = @(x) S(x) + ...
+ polyval(M(i,:), (x - xd(i))).*(x>xd(i)).*(x<=xd(i+1));
+end
+
+% Grafica de la función Spline Cúbica Natural
+% Grafica de la función Spline Cúbica Natural
+plot(xx,S(xx),'k-','linewidth',3)
+plot([-1,1],[0,0],'m-')
+grid on
+grid minor
+xlabel('Datos X')
+ylabel('Funcion (f(x))')
+title('Métodos de Interpolación')
+
+le1=sprintf('Valores dados a interpolar:');
+le2=sprintf('Polinomio de Lagrange grado: ',20);
+le3=sprintf('Spline Cubica de los datos:');
+le4=sprintf('Linea del cero:');
+
+
+legend(le1,le2,le3,le4)
+
+
+
+
